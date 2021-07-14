a90094b22e96116aa6748ddbe18903cfa9e7ecff
Aparna Roychowdhury <aparna.roychowdhury@ibm.com>
2021-07-14 16:56:29 +0530

    deployment ready

--- src/app/constants/REST_API.ts
+++ src/app/constants/REST_API.ts
@@ -1,5 +1,5 @@
 export class REST_API {
-    server_URL: string = 'http://localhost:8080/';
+    server_URL: string = 'https://app-fullstack-aparna.herokuapp.com/';
     authAPIs = {
         register: this.server_URL + 'auth/register',
         login: this.server_URL + 'auth/login'
