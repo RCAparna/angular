a7381364698833cf749747f88f1c63152c8417b8
Aparna Roychowdhury <aparna.roychowdhury@ibm.com>
2021-07-22 23:43:19 +0530

    deployment changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // const path = require("path");
 // const express = require("express");
 // const app = express();
-// app.use(express.static(__dirname + '/angular-material-login-template'));
+// app.use(express.static(__dirname + '/aparna-angular-app'));
 // app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'angular-material-login-template', '../index.html'))
+//   res.sendFile(path.join(__dirname, 'aparna-angular-app', '../index.html'))
 // });
 // // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
@@ -15,11 +15,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-material-login-template'));
+app.use(express.static(__dirname + '/dist/aparna-angular-app'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/angular-material-login-template/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/aparna-angular-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-material-login-template'`, () => {
+  it(`should have as title 'aparna-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-material-login-template');
+    expect(app.title).toEqual('aparna-angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-material-login-template app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('aparna-angular-app app is running!');
   });
 });
