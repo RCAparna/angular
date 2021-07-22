15776ab0c7d35db8e43cbc2ab9c08ea835fc2622
Aparna Roychowdhury <aparna.roychowdhury@ibm.com>
2021-07-22 23:09:27 +0530

    deployment changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/angular-material-login-template'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'angular-material-login-template', 'src/index.html'))
+  res.sendFile(path.join(__dirname, 'angular-material-login-template', '../index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
